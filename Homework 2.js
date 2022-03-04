439052664f1968215c1e780234f6b7a24d8452e5
DavidJulhakyan <61703842+DavidJulhakyan@users.noreply.github.com>
2022-03-04 14:27:24 +0400

    Update Homework 2.js

--- Homework 2.js
+++ Homework 2.js
@@ -9,7 +9,7 @@ alert (Math.abs(a))
 
 
 //2.Given three sides of a triangle. Check whether the triangle is valid or not. Print “yes” if it is valid
-and “no&quot; otherwise. (Triangle is valid if the sum of its two sides are greater than the third side).
+and “no” otherwise. (Triangle is valid if the sum of its two sides are greater than the third side).
 let a = +prompt('first side')
 let b = +prompt('second side')
 let c = +prompt('third side')
@@ -20,22 +20,27 @@ if (a+b>c && a+c>b && b+c>a){
 }
 
 
-//Given a number print its digits count.
+//3.Given a number print its digits count.
 let num = +prompt('type any number')
 let a = 0 
-while (num){
-    a = a + 1
+if (num==0){
+    alert (1)
+} else {
+    while (num){
+    a++
     num = Math.trunc(num/10)
 }
-alert (Math.abs(a))
+    alert (Math.abs(a))
+}
 
 
 //4. Count numbers of digit 9 in a number.
 let num = +prompt('type any number')
 let a = 0 
 while (num){ 
-    if(num%10==9){
-    a = a + 1}
+    if(num%10==9 || num%10==-9){
+    a++
+}
     num = Math.trunc(num/10)
 }
 alert (Math.abs(a))
@@ -48,4 +53,4 @@ while (num){
      a =`${a}`+`${num%10}`
      num = Math.trunc(num/10)
 }
-alert(a)
+alert(+a)
